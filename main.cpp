384a889a30848bc81a7f9b59c37687b5eaf9ed06
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-01-08 17:11:12 +0300

    Added library with versions. Main() invokes major, minor and patch.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include  <iostream>
+#include "vers_lib.h"
 
 using std::cout;
 using std::endl;
 
 int main() {
 
-    cout << " A test run example" << endl;
+    cout << " Major version: " << version_major() << endl;
+    cout << " Minor version: " << version_minor() << endl;
+    cout << " Patch version: " << version_patch() << endl;
 
     return 0;
 }
