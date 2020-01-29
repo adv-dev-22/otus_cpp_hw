78ae65fde5946df41b605ef95656d6a1c5f49c83
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-01-29 13:47:37 +0300

    01 02 hw restructured. No push to bintray. 01 is completely done (unchanged). 02 just started.wq

---
+++ 02/ip_processor.cpp
@@ -0,0 +1,16 @@
+#include "ip_processor.h"
+#include <iostream>
+
+using  std::cout;
+using  std::endl;
+
+IpProcessor::IpProcessor() {
+
+}
+
+void IpProcessor::run() {
+
+    cout << "-- -- --" << endl;
+}
+
+// End of the file
---
+++ 02/ip_processor.h
@@ -0,0 +1,20 @@
+#ifndef _IP_PROCESSOR_H_
+#define _IP_PROCESSOR_H_
+
+/*! Manager class */
+class IpProcessor {
+public:
+    IpProcessor();
+    ~IpProcessor() = default;
+
+    void run();
+
+private:
+
+
+};
+
+#endif  //_IP_PROCESSOR_H_
+
+// End of the file
+
