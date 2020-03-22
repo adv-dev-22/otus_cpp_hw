262ac80d4d28fc29bdee3f540aabb27d512eeb22
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-03-22 21:48:49 +0300

    logging allocator

--- 03_allocator/main_allocator.cpp
+++ 03_allocator/main_allocator.cpp
@@ -1,8 +1,28 @@
+#include "custom_allocator.hpp"
 #include <iostream>
+#include <map>
 
 
 
 int main(int argc, char * argv []) {
+
+    custom_allocator<double> alc;
+    double * darr = alc.allocate(2);
+    darr[1] = 224;
+
+    alc.construct(darr,     2.0);
+    alc.construct(darr + 1, 3.0);
+
+    std::cout << darr[0] << std::endl;
+    std::cout << darr[1] << std::endl;
+
+    alc.destroy(darr);
+
+    alc.deallocate(darr, 2);
+
+
+    std::map<int, int>  standard_map;
+
 
 
 
