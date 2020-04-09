d52f010bf91e8ceb2bc8b822445a338c76d50fa9
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-04-09 09:17:51 +0300

    added empty test

---
+++ 04_sfinae/tests/test_print_ip.cpp
@@ -0,0 +1,19 @@
+#include "../print_ip_04.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
+
+
+TEST(test_all_results, ip) {
+
+
+}
+
+int main(int argc, char *argv[]) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+//std::cout << "full file name: " << full_file_name << std::endl;
+
