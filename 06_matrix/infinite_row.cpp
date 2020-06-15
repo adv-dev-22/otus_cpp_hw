4a86b9db83d63f8e8925d97c6ebdaab5e91fe36f
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-06-15 18:20:14 +0300

    const non_const [] size() is wrong

--- 06_matrix/infinite_row.cpp
+++ 06_matrix/infinite_row.cpp
@@ -2,8 +2,7 @@
 
 template <typename T, T DefaultValue>
 InfiniteRow<T, DefaultValue>::InfiniteRow():
-positions_(0),
-values_(0) {
+values_() {
 
 }
 
@@ -14,18 +13,40 @@ values_(0) {
 //    InfiniteRow & operator = (const InfiniteRow & );
 //    InfiniteRow & operator = (InfiniteRow && );
 
-//    ~InfiniteRow();
+template <typename T, T DefaultValue>
+InfiniteRow<T, DefaultValue>::~InfiniteRow() {
 
-//    const T & operator[] (const size_t index) const;
+}
 
-//    T & operator[] (const size_t index);
+template <typename T, T DefaultValue>
+const T InfiniteRow<T, DefaultValue>::operator[] (const size_t index) const {
 
-//    size_t position(const size_t index) const;
+std::cout << "1: " << index << std::endl;
+    auto itr_idx = values_.find(index);
+    if (values_.end() != itr_idx) return itr_idx->second;
 
-//size_t InfiniteRow<T, Value>::size() const {
-//    return value_.size();
-//}
+    return DefaultValue;
+}
 
+template <typename T, T DefaultValue>
+T& InfiniteRow<T, DefaultValue>::operator[] (const size_t index) {
+
+std::cout << "2: " << index << std::endl;
+    using map_sz_T_itr = typename std::map<size_t, T>::iterator;
+    std::pair<map_sz_T_itr, bool> result = values_.insert(std::pair(index, DefaultValue));
+
+    // Item with such an index exists or default value will be inserted
+    return result.first->second;
+}
+
+template <typename T, T DefaultValue>
+size_t InfiniteRow<T, DefaultValue>::size() const {
+    return values_.size();
+}
+
+//friend bool operator == (const InfiniteRow & inf_row_1, const InfiniteRow & inf_row_2);
 
 
 // End of the file
+
+
