22d9de83fb2c74d490d9b98acdcfd041a8724503
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-06-15 23:58:10 +0300

    cell_observer

--- 06_matrix/infinite_row.cpp
+++ 06_matrix/infinite_row.cpp
@@ -2,7 +2,8 @@
 
 template <typename T, T DefaultValue>
 InfiniteRow<T, DefaultValue>::InfiniteRow():
-values_() {
+values_(),
+cell_observer_(nullptr) {
 
 }
 
@@ -15,28 +16,32 @@ values_() {
 
 template <typename T, T DefaultValue>
 InfiniteRow<T, DefaultValue>::~InfiniteRow() {
-
+    cell_observer_ = nullptr;
 }
 
+//template <typename T, T DefaultValue>
+//const T InfiniteRow<T, DefaultValue>::operator[] (const size_t index) const {
+
+//std::cout << "1: " << index << std::endl;
+//    auto itr_idx = values_.find(index);
+//    if (values_.end() != itr_idx) return itr_idx->second;
+
+//    return DefaultValue;
+//}
+
 template <typename T, T DefaultValue>
-const T InfiniteRow<T, DefaultValue>::operator[] (const size_t index) const {
+CellObserver<T, DefaultValue> & InfiniteRow<T, DefaultValue>::operator[] (const size_t index) {
 
-std::cout << "1: " << index << std::endl;
-    auto itr_idx = values_.find(index);
-    if (values_.end() != itr_idx) return itr_idx->second;
+    cell_observer_->set_index(index);
+    return *cell_observer_;
 
-    return DefaultValue;
-}
+//std::cout << "2: " << index << std::endl;
+//    using map_sz_T_itr = typename std::map<size_t, T>::iterator;
+//    std::pair<map_sz_T_itr, bool> result = values_.insert(std::pair(index, DefaultValue));
 
-template <typename T, T DefaultValue>
-T& InfiniteRow<T, DefaultValue>::operator[] (const size_t index) {
+//    // Item with such an index exists or default value will be inserted
+//    return result.first->second;
 
-std::cout << "2: " << index << std::endl;
-    using map_sz_T_itr = typename std::map<size_t, T>::iterator;
-    std::pair<map_sz_T_itr, bool> result = values_.insert(std::pair(index, DefaultValue));
-
-    // Item with such an index exists or default value will be inserted
-    return result.first->second;
 }
 
 template <typename T, T DefaultValue>
@@ -44,8 +49,24 @@ size_t InfiniteRow<T, DefaultValue>::size() const {
     return values_.size();
 }
 
+template <typename T, T DefaultValue>
+void InfiniteRow<T, DefaultValue>::set_value_(const size_t index, const T & value) {
+
+    //..
+}
+
 //friend bool operator == (const InfiniteRow & inf_row_1, const InfiniteRow & inf_row_2);
 
+template <typename T, T DefaultValue>
+InfiniteRow_Debug<T, DefaultValue>::InfiniteRow_Debug():
+InfiniteRow<T, DefaultValue>() {
+
+}
+
+template <typename T, T DefaultValue>
+InfiniteRow_Debug<T, DefaultValue>::~InfiniteRow_Debug() {
+
+}
 
 // End of the file
 
