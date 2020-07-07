590ea600b461e15140efc61e05751c7380aa6306
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-07-07 20:35:42 +0300

    base_shape_2d

---
+++ 05_editor/base_shape_2d.h
@@ -0,0 +1,21 @@
+#ifndef _BASE_SHAPE_2D_H_
+#define _BASE_SHAPE_2D_H_
+
+namespace GraphicalEditorCore {
+
+class BaseShape2D {
+public:
+    BaseShape2D();
+    virtual ~BaseShape2D();
+
+    // void rotate();
+
+private:
+
+};
+
+} // End of namespace GraphicalEditorCore.
+
+#endif  // _BASE_SHAPE_2D_H_
+
+// End of the file
