1859674bf887897b312aaef161cb02009801c526
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-10-05 19:36:42 +0300

    boost 1.70 travis installation script

--- 09_files/src/fs_file_block_proxy.cpp
+++ 09_files/src/fs_file_block_proxy.cpp
@@ -1,5 +1,18 @@
 #include "fs_file_block_proxy.h"
 #include <fstream>
+#include <iostream>
+#include <boost/uuid/detail/md5.hpp>
+#include <boost/algorithm/hex.hpp>
+
+using boost::uuids::detail::md5;
+
+std::string toString(const md5::digest_type &digest)
+{
+    const auto charDigest = reinterpret_cast<const char *>(&digest);
+    std::string result;
+    boost::algorithm::hex(charDigest, charDigest + sizeof(md5::digest_type), std::back_inserter(result));
+    return result;
+}
 
 
 FsFileBlockProxy::FsFileBlockProxy(const std::string & full_name,
@@ -13,18 +26,43 @@ hash_()
 
 char FsFileBlockProxy::get_hash() const
 {
+//    std::string s;
+
+//      while(std::getline(std::cin, s)) {
+//          md5 hash;
+//          md5::digest_type digest;
+
+//          hash.process_bytes(s.data(), s.size());
+//          hash.get_digest(digest);
+
+//          std::cout << "md5(" << s << ") = " << toString(digest) << '\n';
+//      }
+
+
+
+
     if (!this->is_initialized_)
     {
-        std::vector<char> vec_buffer;
+        const size_t block_size = this->ref_cmpr_optns_.block_size();
 
-        //vec_buffer_.resize(ref_cmpr_optns_.block_size());
+        // Buffer to read in
+        std::vector<char> vec_buffer(block_size, '\0');
 
+        // Open file
         std::ifstream fin(ref_fname_, std::ifstream::binary);
-        //fin
 
-        // ..
+        // Jump to the proper position
+        fin.seekg(idx_offset_ * block_size);
 
-        this->is_initialized_ = true;
+        // In case last block is shorter, it reads only the tail and set eofbit and failbit to 1.
+        fin.read(vec_buffer.data(), block_size);
+
+        for (auto item : vec_buffer)
+        {
+            std::cout << item << " ";
+        } std::cout << std::endl;
+
+        //this->is_initialized_ = true;
     }
 
     return hash_;
--- 09_files/tests/test_file_blocks_number.cpp
+++ 09_files/tests/test_file_blocks_number.cpp
@@ -1,5 +1,6 @@
 #include "fs_comparator_options.h"
 #include "fs_blocks_number_proxy.h"
+#include "fs_file_block_proxy.h"
 #include <gtest/gtest.h>
 #include <iostream>
 #include <fstream>
@@ -42,6 +43,55 @@ TEST(test_blocks_number, basic) {
 
     // Remove temporary file.
     std::remove(test_file_01.c_str());
+
+    // Free memory for command line input simulation.
+    for (int k = 0; k < argc; ++k)
+        delete[] argv[k];
+    delete[] argv;
+}
+
+TEST(test_file_proxy, basic) {
+
+    // Allocate memory for command line input simulation
+    constexpr int argc = 3;
+    char ** argv = new char * [argc];
+    for (int k = 0; k < argc; ++k)
+        argv[k] = new char[100];
+
+    // Copy input values for command line
+    std::strcpy(argv[0], "run_fake");
+    std::strcpy(argv[1], "--blocksize");
+    std::strcpy(argv[2], "3");
+
+    // Uses boost program options
+    FsComparatorOptions fs_optns;
+    fs_optns.parse(argc, argv);
+
+    // Expected value validation
+    EXPECT_EQ(fs_optns.block_size(), 3);
+
+    // Create temporary file.
+    const std::string test_file_02("fs_test_block_hash.txt");
+    std::ofstream ofs;
+    ofs.open(test_file_02, std::ofstream::out);
+    // 95 bytes
+    ofs << "+This file contains several block numbers." << std::endl;
+    ofs << "And explicit blocks number depends on the block size";// << std::endl;
+    ofs.close();
+
+    constexpr size_t offset = 5; //31;
+    FsFileBlockProxy fs_block_proxy(test_file_02, fs_optns, offset);
+
+    std::cout << " from test: " << fs_block_proxy.get_hash() << std::endl;
+
+
+    //const size_t first_request_number  = blocks_number.get();
+    //const size_t second_request_number = blocks_number.get();
+    //EXPECT_EQ(first_request_number,  33);
+    //EXPECT_EQ(second_request_number, 33);
+
+    // Remove temporary file.
+    std::remove(test_file_02.c_str());
 
     // Free memory for command line input simulation.
     for (int k = 0; k < argc; ++k)
